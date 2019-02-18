5353fa83bb10448aa8e03d14f0b3da5de06c5342
Marcel van der Veldt <m.vanderveldt@outlook.com>
2019-02-18 20:14:46 +0100

    fix firstrun

--- autobackup/app/run.sh
+++ autobackup/app/run.sh
@@ -3,19 +3,19 @@
 
 CONFIG_PATH=/data/options.json
 CRON_TASK=$(jq --raw-output ".schedule" $CONFIG_PATH)
+BACKUP_AT_START=$(jq --raw-output ".run_backup_at_startup" $CONFIG_PATH)
+cronjob2=""
 
 # run app immediately if first run detected
-if [ ! -f "/data/firstrun" ]; then
-    echo "First run detected, starting snapshot job"
-    echo -e "firstrun" >> /data/firstrun
-    python /usr/src/app/main.py > /proc/1/fd/1 2>/proc/1/fd/2
+if [ "$BACKUP_AT_START" == "true" ]; then
+    cronjob2="@reboot python /usr/src/app/main.py > /proc/1/fd/1 2>/proc/1/fd/2"
 fi
 
 # apply cron schedule to crontab file
 echo "Using cron schedule: $CRON_TASK"
 rm /etc/crontabs/root
 cronjob="$CRON_TASK python /usr/src/app/main.py > /proc/1/fd/1 2>/proc/1/fd/2"
-echo -e "$cronjob\n\n" >> /etc/crontabs/root
+echo -e "$cronjob\n$cronjob2\n" >> /etc/crontabs/root
 
 # start crond with log level 8 in foreground, output to stderr
 crond -f -d 8
