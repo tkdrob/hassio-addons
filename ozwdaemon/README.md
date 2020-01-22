95ddc06597c98db91140ea04ce9798df6b76f545
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-01-22 23:47:00 +0100

    add ozwdaemon addon

---
+++ ozwdaemon/README.md
@@ -0,0 +1,45 @@
+## Home Assistant Z-Wave over MQTT Integration (Pre-Release)
+
+
+This add-on is a wrapper for the docker image provided here: https://github.com/OpenZWave/qt-openzwave
+OpenZWave's qt-openzwave allows you to control a Z-Wave network over MQTT. It is currently available as a custom component through HACS and will be submitted as an official Home Assistant component once it has matured a bit.
+
+For more information, see the GitHub repository for the hass component:
+https://github.com/cgarwood/homeassistant-zwave_mqtt
+
+
+
+## Installation
+
+The installation of this add-on is pretty straightforward and not different in
+comparison to installing any other Hass.io add-on.
+
+1. [Add my Hass.io add-ons repository][repository] to your Hass.io instance.
+1. Install this add-on.
+1. Click the `Save` button to store your configuration.
+1. Start the add-on.
+1. Check the logs of the add-on to see if everything went well.
+1. Carefully configure the add-on to your preferences, see the official documentation for for that.
+
+
+## Configuration
+
+Some manual configuration is still needed at the moment.
+It is required that you have MQTT setup in HomeAssistant! Hassio includes a Mosquitto MQTT broker addon.
+
+### Option: `broadcast_cmd`
+
+Path to your Z-wave stick/device. Default to //dev/ttyACM0
+
+### Option: `zwave_network_key`
+
+For secure communication it is required that you provide a secure network key.
+Only required if you actually want to include secure devices to your Z-wave mesh,
+
+### Option: `ozw_instance`
+
+Advanced usage only. If you want to connect multiple OZW instances to HomeAssistant.
+Defaults to 1.
+
+
+[repository]: https://github.com/marcelveldt/hassio-addons-repo
---
+++ ozwdaemon/run.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+function file_exists () {
+    # check if a path (partly) exists
+    for f in $1; do
+        [ -e "$f" ] && return 1 || return 0
+        break
+    done
+}
+
+# Migrate existing/previous data from hass config folder
+if [[ $(file_exists "/data/ozwcache_*"; echo $?) -eq 0 ]]; then
+    # try new style ozw_cache file first, fallback to old style zwcfg
+    if [[ $(file_exists "/config/ozwcache_*"; echo $?) -eq 1 ]]; then
+        echo "[INFO] Migrating existing ozw_cache*.xml from /config"
+        cp /config/ozwcache_* /data
+    elif [[ $(file_exists "/config/zwcfg_*"; echo $?) -eq 1 ]]; then
+        echo "[INFO] Migrating existing zwcfg_*.xml from /config"
+        cp /config/zwcfg_* /data
+    fi
+fi
+# Also migrate options.xml if exists
+if [ ! -f "/data/options.xml" ]; then
+    if [ -f "/config/options.xml" ]; then
+        echo "[INFO] Migrating existing options.xml from /config"
+        cp /config/options.xml /data
+    fi
+fi
+
+# TODO: Auto detect path to Z-wave device
+ZWAVE_DEVICE=$(jq --raw-output '.zwave_device' $CONFIG_PATH)
+OZW_INSTANCE=$(jq --raw-output '.ozw_instance' $CONFIG_PATH)
+
+# TODO: Find a better way to supply the Network key
+export OZW_NETWORK_KEY=$(jq --raw-output '.zwave_network_key' $CONFIG_PATH)
+
+MQTT_CONFIG=
+
+# Use Hass.io mqtt services
+if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/services/mqtt)"; then
+    HOST="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.host')"
+    PORT="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.port')"
+    USER="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.username')"
+    PASSWORD="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.password')"
+
+    echo "[INFO] Setup Hass.io mqtt service to ${HOST}"
+
+    (
+        echo "connection main-mqtt"
+        echo "address ${HOST}:${PORT}"
+    ) >> /etc/mosquitto.conf
+
+    if [ -n "${USER}" ]; then
+      (
+          echo "username ${USER}"
+          echo "password ${PASSWORD}"
+      ) >> /etc/mosquitto.conf
+    fi
+
+    # OpenZwave daemon only supports unauthenticated MQTT so we relay messages.
+    # Besides: this adds some stability too as OZW seems to crash if the MQTT server gets unreachable.
+    (
+        echo "topic OpenZWave/${OZW_INSTANCE}/# out"
+        echo "topic # IN OpenZWave/${OZW_INSTANCE}/"
+    ) >> /etc/mosquitto.conf
+else
+    echo "[ERROR] No Hass.io mqtt service found!"
+fi
+
+echo "[INFO] Start internal mqtt broker"
+mosquitto -c /etc/mosquitto.conf &
+
+echo "[INFO] Starting OZW Daemon"
+cd /data
+
+/usr/local/bin/ozwdaemon -s $ZWAVE_DEVICE -c /data -u /data --mqtt-server localhost --mqtt-port 1883 --stop-on-failure --mqtt-instance $OZW_INSTANCE
