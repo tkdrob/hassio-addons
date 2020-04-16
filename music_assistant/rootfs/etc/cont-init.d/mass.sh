082888223af3370ba0fd3529815ae0300ec9f7e8
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-04-16 13:10:09 +0200

    update mass

--- music_assistant/rootfs/etc/cont-init.d/mass.sh
+++ music_assistant/rootfs/etc/cont-init.d/mass.sh
@@ -8,11 +8,11 @@
 # Install latest version from GIT
 if bashio::config.true 'use_nightly'; then
     bashio::log.green "Updating to latest GIT version..."
-    curl https://github.com/marcelveldt/musicassistant/archive/master.zip -LOk -o /tmp/master.zip
     cd /tmp
+    curl https://github.com/marcelveldt/musicassistant/archive/master.zip -LOk
     unzip master.zip
     cd /tmp/musicassistant-master
     python3 setup.py install
-    rm -rf /tmp/musicassistant-master && \
-    rm -f /tmp/master.zip
+    rm -rf musicassistant-master/ && \
+    rm -f master.zip
 fi
