5a2bce56386fbb61da115ef61c7f91bd2dbff068
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-04-16 12:46:46 +0200

    Update mass.sh

--- music_assistant/rootfs/etc/cont-init.d/mass.sh
+++ music_assistant/rootfs/etc/cont-init.d/mass.sh
@@ -8,7 +8,7 @@
 # Install latest version from GIT
 if bashio::config.true 'use_nightly'; then
     bashio::log.green "Updating to latest GIT version..."
-    curl https://github.com/marcelveldt/musicassistant/archive/master.zip -L -o /tmp/master.zip
+    curl https://github.com/marcelveldt/musicassistant/archive/master.zip -LOk -o /tmp/master.zip
     cd /tmp
     unzip master.zip
     cd /tmp/musicassistant-master
